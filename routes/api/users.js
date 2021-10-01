b720231d879d8e5f8e692743781eedd402227624
eitay <eitay@soos.org.il>
2021-10-01 18:51:29 +0300

    adding validation for user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator')
 
 /**
- *@route    GET api/users
- *@desc     Test route
+ *@route    POST api/users
+ *@desc     register a new user
  *@access  Public
  */
 
-router.get('/', (req, res)=>{
-    res.send('User route');
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'please enter a password longer then 6 characters').isLength({min: 6})
+],
+    (req, res)=>{
+    // console.log(req.body);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+    res.send('users route')
 });
 
 module.exports = router;
