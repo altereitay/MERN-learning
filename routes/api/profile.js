56b8ae8850debdfdc9eb349b9f935d3f659d6f36
eitay <eitay@soos.org.il>
2021-10-20 18:36:46 +0300

    get logged in user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../../middleware/auth')
+const Profile = require('../../modules/Profile')
 /**
- *@route    GET api/users
- *@desc     Test route
- *@access  Public
+ *@route   GET api/profile/me
+ *@desc    get current user profile
+ *@access  private
  */
 
-router.get('/', (req, res)=>{
-    res.send('profile route');
+router.get('/me', auth, async (req, res)=>{
+    try {
+        const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar']);
+
+        if(!profile){
+            return res.status(400).json({msg: 'no profile for this user'});
+        }
+        res.json(profile)
+
+    }catch (e) {
+        if (e){
+            console.error(e.message)
+            res.status(500).send('Server error')
+        }
+    }
 });
 
 module.exports = router;
