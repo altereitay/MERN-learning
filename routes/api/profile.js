5fb30b44b1b6b4c95cb71dc1043489534922aa75
eitay <eitay@soos.org.il>
2021-12-27 21:44:16 +0200

    add and delete education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -228,4 +228,70 @@ router.delete('/experience/:exp_id', auth, async (req, res)=>{
     }
 })
 
+/**
+ *@route   PUT api/profile/education
+ *@desc    add profile education
+ *@access  private
+ */
+
+router.put('/education', auth, [
+    check('school', 'school is required').not().isEmpty(),
+    check('degree', 'degree is required').not().isEmpty(),
+    check('from', 'from date is required').not().isEmpty(),
+    check('fieldofstudy', 'filed of study date is required').not().isEmpty()
+], async (req, res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    const {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newEdu = {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    }
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+        profile.education.unshift(newEdu);
+        await profile.save();
+        res.json(profile);
+    }catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server error')
+    }
+})
+
+/**
+ *@route   DELETE api/profile/education/:edu_id
+ *@desc    delete education
+ *@access  private
+ */
+
+router.delete('/education/:edu_id', auth, async (req, res)=>{
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+        const removeIndex = profile.education.map(exp => exp.id).indexOf(req.params.edu_id);
+        profile.education.splice(removeIndex,1);
+        await profile.save();
+        res.json(profile);
+    }catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server error');
+    }
+})
+
 module.exports = router;
