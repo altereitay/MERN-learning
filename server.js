c2bf64defb2fda12bf2e972e32973a927a376df0
eitay <eitay@soos.org.il>
2021-12-31 14:33:37 +0200

    added post route

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.get('/', (req, res)=>{
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/post', require('./routes/api/post'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 app.listen(PORT, ()=> console.log(`server started on port: ${PORT}`))
