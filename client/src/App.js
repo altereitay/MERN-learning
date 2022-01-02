3555f3d56dac724be81e1266b2511a9ec1afb54b
eitay alter <eitay@soos.org.il>
2022-01-03 00:49:59 +0200

    init redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,30 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import {Provider} from "react-redux";
+import store from "./store";
 import './App.css'
 
 //<Route exact path='/login' element={<Login>/}/>
 
 const App = () =>{
     return(
-        <Router>
-            <Fragment>
-                <Navbar />
-                <Routes>
-                    <Route exact path="/" element={<Landing/>} />
-                </Routes>
-                <section className="container">
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Navbar />
                     <Routes>
-                        <Route exact path='register' element={<Register/>}/>
-                        <Route exact path='login' element={<Login/>}/>
+                        <Route exact path="/" element={<Landing/>} />
                     </Routes>
-                </section>
-            </Fragment>
-        </Router>
+                    <section className="container">
+                        <Routes>
+                            <Route exact path='register' element={<Register/>}/>
+                            <Route exact path='login' element={<Login/>}/>
+                        </Routes>
+                    </section>
+                </Fragment>
+            </Router>
+        </Provider>
     );
 }
 
