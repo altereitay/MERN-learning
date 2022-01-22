bfe278a86314c8c6b2d8f0910a190e20cc8fce5d
eitay <eitay@soos.org.il>
2022-01-22 03:34:10 +0200

    moved all backend to same directory

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.get('/', (req, res)=>{
 })
 
 //Define routers
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/users', require('./sever/routes/api/users'))
+app.use('/api/auth', require('./sever/routes/api/auth'))
+app.use('/api/profile', require('./sever/routes/api/profile'))
+app.use('/api/posts', require('./sever/routes/api/posts'))
 
 app.listen(PORT, ()=> console.log(`server started on port: ${PORT}`))
