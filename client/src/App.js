1a1239fc2ca2c8096e312d401d319899bb20442c
eitay <eitay@soos.org.il>
2022-01-22 22:40:40 +0200

    added pages for adding education and experience

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 
 
 if(localStorage.token){
@@ -40,6 +42,8 @@ const App = () =>{
                             <Route path="/dashboard" element={<PrivateRoute component={Dashboard} />}/>
                             <Route path='/create-profile' element={<PrivateRoute component={CreateProfile} />}/>
                             <Route path='/edit-profile' element={<PrivateRoute component={EditProfile} />}/>
+                            <Route path='/add-experience' element={<PrivateRoute component={AddExperience} />}/>
+                            <Route path='/add-education' element={<PrivateRoute component={AddEducation} />}/>
                         </Routes>
                     </section>
                 </Fragment>
--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {getCurrentProfile} from "../../actions/profile";
 
-const DashboardActions = (props) => {
+const DashboardActions = ({profile: {profile: {education, experience}}}) => {
+    useEffect(()=>{
+        getCurrentProfile();
+    }, []);
+    let eduStatus, expStatus;
+    if (education.length === 0 || true){
+        eduStatus = (
+        <Link to="/add-education" className="btn btn-light">
+            <i className="fas fa-graduation-cap text-primary"/> Add Education
+        </Link>
+        )
+    }else{
+         eduStatus = (
+            <Link to="/edit-education" className="btn btn-light">
+                <i className="fas fa-graduation-cap text-primary"/> Edit Education
+            </Link>
+        )}
+    if ( true || experience.length === 0){
+        expStatus = (
+            <Link to="/add-experience" className="btn btn-light">
+                <i className="fas fa-graduation-cap text-primary"/> Add Experience
+            </Link>
+    )
+    }else{
+         expStatus = (
+            <Link to="/edit-experience" className="btn btn-light">
+                <i className="fas fa-graduation-cap text-primary"/> Edit Experience
+            </Link>
+    )}
     return(
         <div className="dash-buttons">
             <Link to="/edit-profile" className="btn btn-light">
                 <i className="fas fa-user-circle text-primary"/> Edit Profile
             </Link>
-            <Link to="/add-experience" className="btn btn-light">
-                <i className="fab fa-black-tie text-primary"/> Add Experience
-            </Link>
-            <Link to="/add-education" className="btn btn-light">
-                <i className="fas fa-graduation-cap text-primary"/> Add Education
-            </Link>
+            {expStatus}
+            {eduStatus}
         </div>
     )
 }
+DashboardActions.propTypes = {
+    profile: PropTypes.object.isRequired
+}
 
-export default DashboardActions;
+const mapStateToProps = state =>({
+    profile: state.profile
+})
+
+export default connect(mapStateToProps, {getCurrentProfile})(DashboardActions);
