be26674e1a22882fde1dd62b4b339878a76ec955
eitay <eitay@soos.org.il>
2022-02-10 07:10:32 +0200

    added likes functionality  have bug with like number

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,9 +3,10 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { connect } from "react-redux";
+import {addLike, removeLike} from "../../actions/post";
 
 
-const PostItem = ({auth, post:{_id, text, name, avatar, user, likes, comments, date}}) =>
+const PostItem = ({addLike, removeLike, auth, post:{_id, text, name, avatar, user, likes, comments, date}}) =>(
     <div className="post bg-white p-1 my-1">
         <div>
             <Link to={`/profile/${user}`}>
@@ -15,13 +16,13 @@ const PostItem = ({auth, post:{_id, text, name, avatar, user, likes, comments, d
         </div>
         <div>
             <p className="my-1">{text}</p>
-            <p className="post-date">Posted on <Moment format='DD/MM/YYY'>{date}</Moment> </p>
-            <button type="button" className="btn btn-light">
-                <i className="fas fa-thumbs-up"/>{' '}
+            <p className="post-date">Posted on <Moment format='DD/MM/YYYY'>{date}</Moment> </p>
+            <button type="button" className="btn btn-light" onClick={()=> {addLike(_id);console.log('add',likes.likes.length, likes.length)}}>
+                <i className="fas fa-thumbs-up" />{' '}
                 {likes.length > 0 &&(<span>{likes.length}</span>)}
             </button>
-            <button type="button" className="btn btn-light">
-                <i className="fas fa-thumbs-down"/>
+            <button type="button" className="btn btn-light" onClick={()=> {removeLike(_id);console.log('remove',likes.likes.length)}}>
+                <i className="fas fa-thumbs-down" />
             </button>
             <Link to={`/post/${_id}`} className="btn btn-primary">
                 Discussion {comments.length > 0 &&(<span className='comment-count'>{comments.length}</span>)}
@@ -33,15 +34,18 @@ const PostItem = ({auth, post:{_id, text, name, avatar, user, likes, comments, d
             )}
         </div>
     </div>
+)
 
 
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired
+    auth: PropTypes.object.isRequired,
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired
 }
 
 const mapStateToProps = (state) => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, {addLike, removeLike})(PostItem);
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,4 @@
-import {GET_POSTS, POST_ERROR} from "../actions/types";
+import { GET_POSTS, POST_ERROR, UPDATED_LIKES } from "../actions/types";
 
 const initialState =  {
     posts: [],
@@ -11,10 +11,11 @@ export default function (state = initialState, action) {
     const {type, payload} = action;
     switch (type) {
         case GET_POSTS:
-            console.log('switch get posts')
             return{ ...state, posts: payload, loading: false};
         case POST_ERROR:
             return {...state, error: payload, loading: false};
+        case UPDATED_LIKES:
+            return {...state, posts: state.posts.map(post => post._id === payload.id ? {...post, likes: payload}: post), loading: false}
         default:
             return state;
     }
