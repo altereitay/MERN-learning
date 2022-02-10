af8e1b3159fd7f5c6cfd0e15244baad9983058ec
eitay <eitay@soos.org.il>
2022-02-10 17:38:33 +0200

    reformatting for bug bounty

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,38 +3,47 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { connect } from "react-redux";
-import {addLike, removeLike} from "../../actions/post";
+import { addLike, removeLike } from "../../actions/post";
 
 
-const PostItem = ({addLike, removeLike, auth, post:{_id, text, name, avatar, user, likes, comments, date}}) =>(
-    <div className="post bg-white p-1 my-1">
-        <div>
-            <Link to={`/profile/${user}`}>
-                <img className="round-img" src={avatar} alt=""/>
-                <h4>{name}</h4>
-            </Link>
-        </div>
-        <div>
-            <p className="my-1">{text}</p>
-            <p className="post-date">Posted on <Moment format='DD/MM/YYYY'>{date}</Moment> </p>
-            <button type="button" className="btn btn-light" onClick={()=> {addLike(_id);console.log('add',likes.likes.length, likes.length)}}>
-                <i className="fas fa-thumbs-up" />{' '}
-                {likes.length > 0 &&(<span>{likes.length}</span>)}
-            </button>
-            <button type="button" className="btn btn-light" onClick={()=> {removeLike(_id);console.log('remove',likes.likes.length)}}>
-                <i className="fas fa-thumbs-down" />
-            </button>
-            <Link to={`/post/${_id}`} className="btn btn-primary">
-                Discussion {comments.length > 0 &&(<span className='comment-count'>{comments.length}</span>)}
-            </Link>
-            {!auth.loading && user === auth.user._id && (
-                <button type="button" className="btn btn-danger">
-                    <i className="fas fa-times"/>
+const PostItem = ({addLike, removeLike, auth, post: {_id, text, name, avatar, user, likes, comments, date}}) => {
+    return (
+        <div className="post bg-white p-1 my-1">
+            <div>
+                <Link to={`/profile/${user}`}>
+                    <img className="round-img" src={avatar} alt=""/>
+                    <h4>{name}</h4>
+                </Link>
+            </div>
+            <div>
+                <p className="my-1">{text}</p>
+                <p className="post-date">Posted on <Moment format='DD/MM/YYYY'>{date}</Moment></p>
+                <button type="button" className="btn btn-light" onClick={() => {
+                    addLike(_id);
+                    console.log('add', likes.likes)
+                }}>
+                    <i className="fas fa-thumbs-up"/>{' '}
+                    {likes.length > 0 &&
+                    (<p>{likes.likes}</p>)}
                 </button>
-            )}
+                <button type="button" className="btn btn-light" onClick={() => {
+                    removeLike(_id);
+                    console.log('remove', likes.likes.length)
+                }}>
+                    <i className="fas fa-thumbs-down"/>
+                </button>
+                <Link to={`/post/${_id}`} className="btn btn-primary">
+                    Discussion {comments.length > 0 && (<span className='comment-count'>{comments.length}</span>)}
+                </Link>
+                {!auth.loading && user === auth.user._id && (
+                    <button type="button" className="btn btn-danger">
+                        <i className="fas fa-times"/>
+                    </button>
+                )}
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 
 PostItem.propTypes = {
