2d0fc1202df651028957cbd45eebbff44be52e99
eitay <eitay@soos.org.il>
2022-03-01 17:26:27 +0200

    WIP on edit experience

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -1,10 +1,13 @@
-import React, {Fragment} from "react";
+import React, { Fragment, useState } from "react";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
 import {deleteEducation} from "../../actions/profile";
 import {connect} from "react-redux";
+import { Link } from "react-router-dom";
+import { Header, Segment, TransitionablePortal } from "semantic-ui-react";
 
 const Education = ({education, deleteEducation}) =>{
+    const [open, setOpen] = useState(false);
     const educations = education.map(edu =>(
         <tr key={edu._id}>
             <td>{edu.school}</td>
@@ -13,6 +16,23 @@ const Education = ({education, deleteEducation}) =>{
                 <Moment format='DD/MM/YYYY'>{edu.from}</Moment> - {
                 edu.to === undefined? ('Current') : (<Moment format='DD/MM/YYYY'>{edu.to}</Moment>
                 )}
+            </td>
+            <td>
+                <TransitionablePortal
+                    closeOnTriggerClick
+                    onOpen={()=>setOpen(true)}
+                    onClose={()=>setOpen(false)}
+                    openOnTriggerClick
+                    trigger={
+                        <button className='btn btn-light' >Edit Education</button>
+                    }
+                >
+                    <Segment style={{ left: '40%', position: 'fixed', top: '50%', zIndex: 1000, background: 'coral' }}>
+                        <Header>This is an example portal</Header>
+                        <p>Portals have tons of great callback functions to hook into.</p>
+                        <p>To close, simply click the close button or click away</p>
+                    </Segment>
+                </TransitionablePortal>
             </td>
             <td>
                 <button className='btn btn-danger' onClick={()=>deleteEducation(edu._id)}>Delete</button>
@@ -28,6 +48,7 @@ const Education = ({education, deleteEducation}) =>{
                     <th>School</th>
                     <th className='hide-sm'>Degree</th>
                     <th className='hide-sm'>Years</th>
+                    <th/>
                     <th/>
                 </tr>
                 </thead>
--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -1,25 +1,30 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
-import {deleteExperience} from "../../actions/profile";
+import { deleteExperience } from "../../actions/profile";
 import { connect } from "react-redux";
+import EditExperience from "../profile-forms/EditExperience";
 
-const Experience = ({experience ,deleteExperience}) =>{
-    const experiences = experience.map(exp =>(
+
+const Experience = ({experience, deleteExperience}) => {
+    const experiences = experience.map(exp => (
         <tr key={exp._id}>
             <td>{exp.company}</td>
             <td className='hide-sm'>{exp.title}</td>
             <td>
                 <Moment format='DD/MM/YYYY'>{exp.from}</Moment> - {
-                exp.to === undefined? ('Current') : (<Moment format='DD/MM/YYYY'>{exp.to}</Moment>
+                exp.to === undefined ? ('Current') : (<Moment format='DD/MM/YYYY'>{exp.to}</Moment>
                 )}
             </td>
             <td>
-                <button className='btn btn-danger' onClick={()=>deleteExperience(exp._id)}>Delete</button>
+                <EditExperience exp={exp}/>
+            </td>
+            <td>
+                <button className='btn btn-danger' onClick={() => deleteExperience(exp._id)}>Delete</button>
             </td>
         </tr>
     ));
-    return(
+    return (
         <Fragment>
             <h2 className='my-2'>Experience credentials</h2>
             <table className='table'>
@@ -28,6 +33,7 @@ const Experience = ({experience ,deleteExperience}) =>{
                     <th>Company</th>
                     <th className='hide-sm'>Title</th>
                     <th className='hide-sm'>Years</th>
+                    <th/>
                     <th/>
                 </tr>
                 </thead>
